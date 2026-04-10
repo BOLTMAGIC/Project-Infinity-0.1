e7c463b245466cce82c331fc3414d7c8774244bb
BOLT_M4G1C <kow656@gmail.com>
2026-04-10 22:34:50 +0200

    code cleanup

--- kubejs/server_scripts/enderio/add_alloy_smelter.js
+++ kubejs/server_scripts/enderio/add_alloy_smelter.js
@@ -1,29 +1,73 @@
 ServerEvents.recipes((event) => {
-  event.custom({
-    type: 'enderio:alloy_smelting',
-    result: { item: 'kubejs:crystalline_alloy' },
-    ingredients: [
-      { item: 'minecraft:gold_ingot' },
-      { item: 'enderio:pulsating_powder' },
-      { item: 'actuallyadditions:diamatine_crystal_shard' },
-    ],
-    energy: 10000,
-  });
+  event
+    .custom({
+      type: 'enderio:alloy_smelting',
+      energy: 10000,
+      inputs: [
+        {
+          count: 1,
+          ingredient: {
+            item: 'minecraft:gold_ingot',
+          },
+        },
+        {
+          count: 1,
+          ingredient: {
+            item: 'enderio:pulsating_powder',
+          },
+        },
+        {
+          count: 1,
+          ingredient: {
+            item: 'actuallyadditions:diamatine_crystal_shard',
+          },
+        },
+      ],
+      result: {
+        item: 'kubejs:crystalline_alloy',
+      },
+    })
+    .id('kubejs:crystalline_alloy_from_alloy_smelter');
 
-  event.custom({
-    type: 'enderio:alloy_smelting',
-    result: { item: 'kubejs:azure_silver_ingot' },
-    ingredients: [{ item: 'kubejs:azure_silver_ore' }],
-    energy: 10000,
-  });
+  event
+    .custom({
+      type: 'enderio:alloy_smelting',
+      energy: 10000,
+      inputs: [
+        {
+          count: 1,
+          ingredient: {
+            item: 'kubejs:azure_silver_ore',
+          },
+        },
+      ],
+      result: {
+        item: 'kubejs:azure_silver_ingot',
+      },
+    })
+    .id('kubejs:azure_silver_ingot_from_alloy_smelter');
 
-  event.custom({
-    type: 'enderio:alloy_smelting',
-    result: { item: 'thermal:steel_ingot' },
-    ingredients: [
-      { item: '3x minecraft:coal' },
-      { item: 'minecraft:iron_ingot' },
-    ],
-    energy: 10000,
-  });
+  event
+    .custom({
+      type: 'enderio:alloy_smelting',
+      energy: 10000,
+      inputs: [
+        {
+          count: 3,
+          ingredient: {
+            item: 'minecraft:coal',
+          },
+        },
+        {
+          count: 1,
+          ingredient: {
+            item: 'minecraft:iron_ingot',
+          },
+        },
+      ],
+      result: {
+        item: 'thermal:steel_ingot',
+      },
+    })
+    .id('kubejs:thermal_steel_ingot_from_alloy_smelter');
 });
--- kubejs/server_scripts/enderio/add_sag_mill.js
+++ kubejs/server_scripts/enderio/add_sag_mill.js
@@ -1,50 +1,59 @@
 ServerEvents.recipes((event) => {
-  sag_milling(event, 'compressium:stone_1', '2x compressium:cobblestone_1');
-  sag_milling(event, 'compressium:stone_2', '2x compressium:cobblestone_2');
-  sag_milling(event, 'compressium:stone_3', '2x compressium:cobblestone_3');
-  sag_milling(event, 'compressium:cobblestone_1', '2x compressium:gravel_1');
-  sag_milling(event, 'compressium:cobblestone_2', '2x compressium:gravel_2');
-  sag_milling(event, 'compressium:cobblestone_3', '2x compressium:gravel_3');
-  sag_milling(event, 'compressium:gravel_1', '2x compressium:sand_1');
-  sag_milling(event, 'compressium:gravel_2', '2x compressium:sand_2');
-  sag_milling(event, 'compressium:gravel_3', '2x compressium:sand_3');
-  sag_milling(event, 'minecraft:sand', '2x exdeorum:dust');
-  sag_milling(
-    event,
-    'compressium:netherrack_1',
-    '2x compressium:crushed_netherrack_1'
-  );
+  sag_milling(event, 'compressium:stone_1', ['compressium:cobblestone_1', 2]);
+  sag_milling(event, 'compressium:stone_2', ['compressium:cobblestone_2', 2]);
+  sag_milling(event, 'compressium:stone_3', ['compressium:cobblestone_3', 2]);
+  sag_milling(event, 'compressium:cobblestone_1', ['compressium:gravel_1', 2]);
+  sag_milling(event, 'compressium:cobblestone_2', ['compressium:gravel_2', 2]);
+  sag_milling(event, 'compressium:cobblestone_3', ['compressium:gravel_3', 2]);
+  sag_milling(event, 'compressium:gravel_1', ['compressium:sand_1', 2]);
+  sag_milling(event, 'compressium:gravel_2', ['compressium:sand_2', 2]);
+  sag_milling(event, 'compressium:gravel_3', ['compressium:sand_3', 2]);
+  sag_milling(event, 'minecraft:sand', ['exdeorum:dust', 2]);
+  sag_milling(event, 'compressium:netherrack_1', [
+    'compressium:crushed_netherrack_1',
+    2,
+  ]);
   sag_milling(
     event,
     'compressium:endstone_1',
-    '2x compressium:crushed_end_stone_1'
+    ['compressium:crushed_end_stone_1', 2],
+    2
   );
-  sag_milling(event, 'kubejs:blaze_gold_ingot', 'kubejs:blaze_gold_dust');
-  sag_milling(event, 'kubejs:azure_silver_ingot', 'kubejs:azure_silver_dust');
-  sag_milling(
-    event,
-    'kubejs:azure_electrum_ingot',
-    'kubejs:azure_electrum_dust'
-  );
-  sag_milling(event, 'kubejs:arcmetal_ore', '2x kubejs:raw_arcmetal');
-  sag_milling(event, 'kubejs:solarmetal_ore', '2x kubejs:raw_solarmetal');
-  sag_milling(event, 'kubejs:plasteel_ore', '2x kubejs:raw_plasteel');
-  sag_milling(event, 'kubejs:voidmetal_ore', '2x kubejs:raw_voidmetal');
+  sag_milling(event, 'kubejs:blaze_gold_ingot', ['kubejs:blaze_gold_dust', 1]);
+  sag_milling(event, 'kubejs:azure_silver_ingot', [
+    'kubejs:azure_silver_dust',
+    1,
+  ]);
+  sag_milling(event, 'kubejs:azure_electrum_ingot', [
+    'kubejs:azure_electrum_dust',
+    1,
+  ]);
+  sag_milling(event, 'kubejs:arcmetal_ore', ['kubejs:raw_arcmetal', 2]);
+  sag_milling(event, 'kubejs:solarmetal_ore', ['kubejs:raw_solarmetal', 2]);
+  sag_milling(event, 'kubejs:plasteel_ore', ['kubejs:raw_plasteel', 2]);
+  sag_milling(event, 'kubejs:voidmetal_ore', ['kubejs:raw_voidmetal', 2]);
 });
 
-function sag_milling(event, input, output) {
+function sag_milling(event, input, output, outputCount) {
+  // support passing output as either a string or an array [item, count]
+  let itemName = output;
+  let count = outputCount ? outputCount : 1;
+  if (Array.isArray(output)) {
+    itemName = output[0];
+    if (output[1] !== undefined) count = output[1];
+  }
+
   event
     .custom({
       type: 'enderio:sag_milling',
       input: { item: input },
       outputs: [
         {
-          item: {
-            item: output,
-          },
+          item: itemName,
+          count: count,
         },
       ],
       energy: 5000,
     })
-    .id('sag_milling_' + output);
+    .id('sag_milling_' + itemName.replace(':', '_'));
 }

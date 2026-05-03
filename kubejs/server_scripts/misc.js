0f3682f66295a3253dfcbb3de7114f7980b6abcb
Minimaxi28 <minimaxi@outlook.com>
2026-05-03 18:49:19 +0200

    change mysticalagriculture:master_infusion_crystal recipe
    remove 1 of them because there is no reason for it to exist
    
    use tag matc:supremium to make it be able to use the mysticalagriculture:master_infusion_crystal for the craft too

--- kubejs/server_scripts/misc.js
+++ kubejs/server_scripts/misc.js
@@ -84,23 +84,11 @@ ServerEvents.recipes((event) => {
     'kubejs:infinity_4',
     'mysticalagriculture:prosperity_shard',
     'kubejs:infinity_4',
-    'matc:supremium_crystal',
+    '#matc:supremium',
     'kubejs:infinity_4',
     'mysticalagriculture:prosperity_shard',
     'kubejs:infinity_4',
     'mysticalagriculture:prosperity_shard',
-  ]);
-
-  create3x3(event, 'mysticalagriculture:master_infusion_crystal', [
-    'kubejs:infinity_4',
-    'mysticalagriculture:prosperity_shard',
-    'kubejs:infinity_4',
-    'mysticalagriculture:prosperity_shard',
-    'matc:supremium_crystal',
-    'mysticalagriculture:prosperity_shard',
-    'kubejs:infinity_4',
-    'mysticalagriculture:prosperity_shard',
-    'kubejs:infinity_4',
   ]);
 
   //mod: enchantinginfuser

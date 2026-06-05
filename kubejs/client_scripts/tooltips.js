9bafb1ea3ff218c468e9fb27cc3c0bc673f9a1b1
Minimaxi28 <minimaxi@outlook.com>
2026-06-06 00:29:46 +0200

    Add kubejs:extra_data_model_uncrafter
    Gives back the Data Models used in the Extra Data Model

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -536,4 +536,10 @@ ItemEvents.tooltip((event) => {
     ['thermal:compost'],
     Text.translate('tooltip.thermal.compost').green()
   );
+
+  event.addAdvanced(['kubejs:extra_data_model_uncrafter'], (item, advanced, text) => {
+    text.add(1, Text.translate('tooltip.kubejs.extra_data_model_uncrafter_1'));
+    text.add(2, Text.translate('tooltip.kubejs.extra_data_model_uncrafter_2'));
+    text.add(3, Text.translate('tooltip.kubejs.extra_data_model_uncrafter_3').red());
+  });
 });

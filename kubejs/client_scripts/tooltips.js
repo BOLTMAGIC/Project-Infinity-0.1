1352a7e324243704a139c836214e394909df83fe
Minimaxi28 <minimaxi@outlook.com>
2026-06-05 23:02:46 +0200

    Add outer_end:ancient_ice location tooltip

--- kubejs/client_scripts/tooltips.js
+++ kubejs/client_scripts/tooltips.js
@@ -294,9 +294,9 @@ ItemEvents.tooltip((event) => {
 
 
   // TODO FINISHME: Tell them the biome it spawns in - used in supercooled ice
-  event.addAdvanced(['outer_end:ancient_ice']), (item, advanced, text) => {
+  event.addAdvanced(['outer_end:ancient_ice'], (item, advanced, text) => {
     text.add(1, Text.translate('tooltip.outer_end.ancient_ice').green());
-  }
+  });
 
   // Credit Recipes
   const kemCredits = [

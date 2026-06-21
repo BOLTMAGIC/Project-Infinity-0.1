9828efa21c3c943187a1d807b04ae14df6f62f0d
Minimaxi28 <minimaxi@outlook.com>
2026-06-21 12:39:18 +0200

    Revert "Change Project E AOE disabling message"
    This reverts commit 80ceafcd6405fed7f08a5a8450f96f195c1840d7.

--- kubejs/server_scripts/scripts/commands/disable_red_morningstar_aoe.js
+++ kubejs/server_scripts/scripts/commands/disable_red_morningstar_aoe.js
@@ -2,18 +2,18 @@ ServerEvents.commandRegistry(event => {
   const { commands: Commands } = event
 
   event.register(
-    Commands.literal('project_e_aoe')
+    Commands.literal('red_morningstar_aoe')
       .requires(src => src.hasPermission(0))
       .executes(ctx => {
         const { player, server } = ctx.source
         const pData = player.persistentData
         if (player == null) return 0
 
-        pData['project_e_aoe_disabled'] = !pData['project_e_aoe_disabled']
+        pData['red_morningstar_aoe_disabled'] = !pData['red_morningstar_aoe_disabled']
         
         player.tell([
-          Text.literal('Project E AOE is now : '),
-          pData['project_e_aoe_disabled'] == false ? Text.green('Enabled') : Text.red('Disabled')
+          Text.literal('Red Morningstar AOE is now : '),
+          pData['red_morningstar_aoe_disabled'] == false ? Text.green('Enabled') : Text.red('Disabled')
         ]);
 
         return 1
@@ -22,7 +22,7 @@ ServerEvents.commandRegistry(event => {
 })
 
 PlayerEvents.loggedIn(event => {
-  if (event.player.persistentData['project_e_aoe_disabled'] == null) {
-    event.player.persistentData['project_e_aoe_disabled'] = true;
+  if (event.player.persistentData['red_morningstar_aoe_disabled'] == null) {
+    event.player.persistentData['red_morningstar_aoe_disabled'] = true;
   }
 })
